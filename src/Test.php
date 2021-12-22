97d3fe343fde48c0d2477a3509c9b0dfcfd0947c
李伟 <liwei_1991@126.com>
2021-12-23 00:17:23 +0800

    添加db操作支持filter

---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Leegode\ThinkFilter;
+
+use think\db\Query;
+
+class Test extends Query
+{
+    use ThinkFilter;
+}
--- src/ThinkFilter.php
+++ src/ThinkFilter.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of theleegode/think-filter.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ *
+ */
+
 namespace Leegode\ThinkFilter;
 
 use think\facade\Config;
@@ -7,41 +16,46 @@ use think\facade\Config;
 trait ThinkFilter
 {
     protected $filterInstance;
+
     /**
-     * 模型搜索器入口
+     * 模型搜索器入口.
+     *
      * @param $query
-     * @param  array|string  $input
+     * @param array|string $input
      * @param $filter
      */
-    public function scopeFilter($query,  $input=[],$filter=null)
+    public function scopeFilter($query, $input = [], $filter = null)
     {
-        if ($filter === null) {
-            $filter = $this->getClass();
-        }
-        $filterInstance = new $filter($query, $input);
-        $this->filterInstance =$filterInstance;
-
-        return $filterInstance->apply();
-
+       $this->filter($query,$input,$filter);
     }
 
+    public function filter($query=null, $input = [], $filter = null)
+    {
+        if(null===$query){
+            $query = $this;
+        }
+        if (null === $filter) {
+            $filter = $this->getClass();
+        }
+        $this->filterInstance = new $filter($query, $input);
+
+        return $this->filterInstance->apply();
+    }
 
     private function getClass()
     {
         return method_exists($this, 'getFilterClass') ? $this->getFilterClass() : $this->getFilter();
     }
 
-
     private function getFilter($filter = null)
     {
-        if ($filter === null) {
+        if (null === $filter) {
             $filter = Config::get('filter.namespace', 'app\\filters\\').class_basename($this).'Filter';
-            if(!class_exists($filter)){
-                $filter =  Config::get('filter.base_filter', 'Leegode\\ThinkFilter\\BaseFilter');
+            if (!class_exists($filter)) {
+                $filter = Config::get('filter.base_filter', 'Leegode\\ThinkFilter\\BaseFilter');
             }
         }
 
         return $filter;
     }
-
 }
