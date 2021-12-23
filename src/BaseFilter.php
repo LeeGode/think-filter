5ddffa12ed8350966490858bf7c57aab389ed446
Leegode <LeeGode@users.noreply.github.com>
2021-12-23 05:37:21 +0000

    Apply fixes from StyleCI

--- src/BaseFilter.php
+++ src/BaseFilter.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the leegode/think-filter.
+ * This file is part of theleegode/think-filter.
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
