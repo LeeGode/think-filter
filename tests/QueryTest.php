93be67b7e0f43e4e2bdad41736f5313751477d8c
Leegode <LeeGode@users.noreply.github.com>
2021-12-23 09:23:30 +0000

    Apply fixes from StyleCI

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -1,7 +1,15 @@
 <?php
-namespace Leegode\ThinkFilter\Tests;
 
-use think\db\Query;
+/*
+ * This file is part of the leegode/think-filter.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ *
+ */
+
+namespace Leegode\ThinkFilter\Tests;
 
 class QueryTest extends \PHPUnit\Framework\TestCase
 {
